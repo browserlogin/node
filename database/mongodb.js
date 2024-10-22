46f89b518e1f7fbe72a6800297db3218e0dc204d
browserlogon <browserlaptooplogin@gmail.com>
2024-10-22 14:00:43 +0530

    env file use

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,21 +1,21 @@
 const helper = require('../helper/helper');
-//const dotenv = require('dotenv').config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 //mongodb url with database 'mongodb://localhost:27017/databasename'
-const MONGO_URL = 'mongodb://localhost:27017/rohit';
+//const MONGO_URL = 'mongodb://localhost:27017/rohit';//local
 helper.messageInConsole('mongo file');
-//const m =process.env.MONGO_URL;
-mongoose.connect(MONGO_URL, {
-                
-            });
+//const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(process.env.MONGO_URL, {
+
+});
 const db = mongoose.connection;
-db.on('connected',() =>{
+db.on('connected', () => {
     helper.messageInConsole('mongo db server connect');
 });
-db.on('error',(err) =>{
-    helper.messageInConsole('mongo db server Error'+err);
+db.on('error', (err) => {
+    helper.messageInConsole('mongo db server Error' + err);
 });
-db.on('disconnected',() =>{
+db.on('disconnected', () => {
     helper.messageInConsole('mongo db server disconnect');
 });
 module.exports = db;
